b581e05602b43a7eb7b8e52fda120774ffcfacb9
KostasRomanidis <mourdaris@gmail.com>
2017-04-08 14:26:52 +0300

    API constrain-routesfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: 'api' do
-    namespace :v1 do
+    namespace :v1, constraints: ApiConstraint.new(version: 1) do
+      resources :users, only: [:index, :create]
       resources :products, :customers do
         resources :purchases
       end
     end
   end
+
+  # mount Knock::Engine => "/knock"
 end
